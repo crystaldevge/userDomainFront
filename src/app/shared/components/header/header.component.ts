4ae76734364d82f42abc64b3657de2f3fe74c635
MateCrystal <m.bukia@crystal.ge>
2025-06-23 15:19:30 +0400

    Remove login from front

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
-import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 
 
 @Component({
@@ -20,19 +19,5 @@ export class HeaderComponent {
   ngOnInit(): void {
     
   }
-
-logout(): void {
-  const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-    width: '300px',
-    data: { message: "Do you want to log out?" }
-  });
-
-  dialogRef.afterClosed().subscribe(result => {
-    if (result === true) {
-      this.authService.logout(); // Clear the token
-      this.router.navigate(['/login']); // Redirect to the login page
-    }
-  });
-}
 
 }
